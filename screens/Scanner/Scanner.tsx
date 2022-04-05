6ac7c2c41ab59e20f01e0c9a0425741f7c4888b0
Peter <p.dyumin@dunice.net>
2022-04-05 22:19:11 +0300

    Update the Scanner screen

--- screens/Scanner/Scanner.tsx
+++ screens/Scanner/Scanner.tsx
@@ -2,14 +2,16 @@ import React, { memo, useEffect, useState } from 'react';
 import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Loader from '../../components/Loader';
 import styles from './styles';
 
 function Scanner(): React.ReactElement {
   const [havePermission, setHavePermission] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const [scanned, setScanned] = useState<boolean>(false);
 
   useEffect(
-    () => {
+    (): void => {
       async function getPermissions() {
         const result = await BarCodeScanner.requestPermissionsAsync();
         // TODO: check permissions
@@ -24,13 +26,16 @@ function Scanner(): React.ReactElement {
 
   return (
     <View style={styles.container}>
-      { havePermission && !scanned && (
+      { loading && (
+        <Loader />
+      ) }
+      { !loading && havePermission && !scanned && (
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleScanned}
           style={StyleSheet.absoluteFillObject}
         />
       ) }
-      { !havePermission && (
+      { !loading && !havePermission && (
         <Text>
           Permission required
         </Text>
