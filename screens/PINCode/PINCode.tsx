67a3c8af36949d9f098a312fc75a42f1f76331a1
Peter <p.dyumin@dunice.net>
2022-04-06 20:48:22 +0300

    Add some logic to the PIN Code screen

--- screens/PINCode/PINCode.tsx
+++ screens/PINCode/PINCode.tsx
@@ -1,14 +1,43 @@
-import React, { memo } from 'react';
+import React, {
+  memo,
+  useEffect,
+  useState,
+} from 'react';
 import { Text, View } from 'react-native';
 
+import { getValue, KEYS } from '../../utilities/storage';
+import Loader from '../../components/Loader';
 import styles from './styles';
 
 function PINCode(): React.ReactElement {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [PIN, setPIN] = useState<number>();
+
+  useEffect(
+    (): void => {
+      async function checkPin() {
+        const pinValue = await getValue<number>(KEYS.pin);
+        if (pinValue) {
+          setPIN(pinValue);
+        }
+        setLoading(false);
+      }
+
+      checkPin();
+    },
+    [],
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        PIN Code
-      </Text>
+      { loading && (
+        <Loader />
+      ) }
+      { !loading && (
+        <Text style={styles.title}>
+          { `${PIN ? 'Has PIN' : 'Set up PIN'}` }
+        </Text>
+      ) }
     </View>
   );
 }
