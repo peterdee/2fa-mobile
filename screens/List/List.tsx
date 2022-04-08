83beee26fe6e6ba2f5b8168a82c5e2993acd497a
Peter <p.dyumin@dunice.net>
2022-04-08 18:07:29 +0300

    Work on List

--- screens/List/List.tsx
+++ screens/List/List.tsx
@@ -1,14 +1,40 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+
+import { getValue, KEYS } from '../../utilities/storage';
+import { TokenEntry } from '../../types/models';
 
 import styles from './styles';
 
 function List(): React.ReactElement {
+  const [list, setList] = useState<TokenEntry[]>([]);
+
+  useEffect(
+    (): void => {
+      async function getTokens(): Promise<void> {
+        const entries = await getValue<TokenEntry[]>(KEYS.tokens);
+        if (entries) {
+          setList(entries);
+        }
+      }
+
+      getTokens();
+    },
+    [],
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        List
-      </Text>
+      { list.length > 0 && (
+        <Text style={styles.title}>
+          { `List: ${JSON.stringify(list)}` }
+        </Text>
+      ) }
+      { list.length === 0 && (
+        <Text style={styles.title}>
+          Nothing to display
+        </Text>
+      ) }
     </View>
   );
 }
