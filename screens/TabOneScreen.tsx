006331fb34ca03c916a9d411fe6b91244c13cf0b
Peter <p.dyumin@dunice.net>
2022-04-14 21:33:57 +0300

    Refresh OTP value every 30 seconds

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import {
   Pressable,
   StyleSheet,
@@ -26,33 +26,55 @@ const styles = StyleSheet.create({
   },
 });
 
+const DISPLAY_TIME = 30;
+
 function TabOneScreen(): React.ReactElement {
-  const [code, setCode] = useState<string>('');
+  const [code, setCode] = useState<number>();
+  const [displayTime, setDisplayTime] = useState<number>(DISPLAY_TIME);
   const [scanned, setScanned] = useState<boolean>(false);
   const [scanning, setScanning] = useState<boolean>(false);
+  const [token, setToken] = useState<string>('');
 
   const handleScan = async (): Promise<void> => {
     setScanning(true);
     await BarCodeScanner.requestPermissionsAsync();
   };
   const handleScanned = (result: BarCodeScannerResult): void => {
-    console.log('scanned', JSON.stringify(result));
-    setCode(result.data);
+    setCode(totp(result.data));
     setScanned(true);
+    setToken(result.data);
   };
+
+  useEffect(
+    (): (() => void) => {
+      if (displayTime === 0) {
+        setCode(totp(token));
+        setDisplayTime(DISPLAY_TIME);
+      }
+
+      const timer = setTimeout(
+        (): void => setDisplayTime((state): number => state - 1),
+        1000,
+      );
+
+      return (): void => {
+        clearTimeout(timer);
+      };
+    },
+    [
+      displayTime,
+      token,
+    ],
+  );
 
   return (
     <View style={styles.container}>
       { !scanning && !scanned && (
-        <>
-          <Text style={styles.title}>Tab One</Text>
-          <View style={styles.separator} />
-          <Pressable onPress={handleScan}>
-            <Text style={styles.title}>
-              Start scanning
-            </Text>
-          </Pressable>
-        </>
+        <Pressable onPress={handleScan}>
+          <Text style={styles.title}>
+            Start scanning
+          </Text>
+        </Pressable>
       ) }
       { scanning && !scanned && (
         <BarCodeScanner
@@ -62,7 +84,7 @@ function TabOneScreen(): React.ReactElement {
       ) }
       { scanned && (
         <Text style={styles.title}>
-          { `Code: ${totp(code)}` }
+          { `Code: ${code}, time: ${displayTime}` }
         </Text>
       ) }
     </View>
